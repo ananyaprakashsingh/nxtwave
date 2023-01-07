bedea95a25dfd2b1c28a1ef121e35d82387156ed
ananyaprakashsingh <ananyaprakashsingh@gmail.com>
2023-01-08 01:54:19 +0530

    Add Form validation in Add Item page.

--- src/pages/form/Form.js
+++ src/pages/form/Form.js
@@ -7,23 +7,90 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function Form() {
     const navigate = useNavigate();
-    const [item, setItem] = useState({ title: '', link: '', url: '', tag: '', category: '', description: '' })
+    const [item, setItem] = useState({ title: '', link: '', url: '', tag: 'User', category: '', description: '' })
+
+    function isURL(str) {
+        var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+            '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+        return pattern.test(str);
+    }
+
     const handleSubmit = async () => {
         try {
+            for (var key in item) {
+
+                if (item[key] === "") {
+                    toast.error(`${key.charAt(0).toUpperCase() + key.slice(1)} is required.`, {
+                        position: "bottom-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored"
+                    })
+                    return;
+                }
+            }
+
+            if (!isURL(item['link'])) {
+                toast.error(`Link is invalid.`, {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored"
+                })
+                return;
+            }
+
+            if (!isURL(item['url'])) {
+                toast.error(`Icon Url is invalid.`, {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored"
+                })
+                return;
+            }
+
             const res = await axios.get('https://media-content.ccbp.in/website/react-assignment/add_resource.json', item);
-            console.log("Res", res);
+
             toast.success('Added Successfully!', {
-                position: "top-right",
+                position: "bottom-center",
                 autoClose: 5000,
                 hideProgressBar: false,
                 closeOnClick: true,
                 pauseOnHover: true,
                 draggable: true,
                 progress: undefined,
-                theme: "light",
+                theme: "colored",
             });
+            setItem(({ title: '', link: '', url: '', tag: 'User', category: '', description: '' }))
         } catch (err) {
-            toast("Failed!");
+            toast.error("Failed", {
+                position: "bottom-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored"
+            })
+
             console.log(err.response);
         }
 
@@ -39,7 +106,7 @@ function Form() {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme="light"
+        theme="colored"
     />
         <div className='form'>
 
@@ -48,17 +115,17 @@ function Form() {
                 <div className='form-container'>
 
                     <div className='item-details'>Item Details</div>
-                    <div className='dv-mr'><label className='lb'>ITEM TITLE</label><input type="text" className='inp' value={item.title} name="title" onChange={(e) => setItem({ ...item, [e.target.name]: e.target.value })} /></div>
-                    <div className='dv-mr'><label className='lb'>LINK</label><input type="url" className='inp inp-link' value={item.link} name="link" onChange={(e) => setItem({ ...item, [e.target.name]: e.target.value })} /></div>
-                    <div className='dv-mr'><label className='lb'>ICON URL</label><input type="url" className='inp inp-link' value={item.url} name="url" onChange={(e) => setItem({ ...item, [e.target.name]: e.target.value })} /></div>
+                    <div className='dv-mr'><label className='lb'>ITEM TITLE</label><input type="text" className='inp' value={item.title} name="title" onChange={(e) => setItem({ ...item, [e.target.name]: e.target.value })} required /></div>
+                    <div className='dv-mr'><label className='lb'>LINK</label><input type="url" className='inp inp-link' value={item.link} name="link" onChange={(e) => setItem({ ...item, [e.target.name]: e.target.value })} required /></div>
+                    <div className='dv-mr'><label className='lb'>ICON URL</label><input type="url" className='inp inp-link' value={item.url} name="url" onChange={(e) => setItem({ ...item, [e.target.name]: e.target.value })} required /></div>
                     <div className='dv-mr '> <label className='lb'>TAG NAME</label>
                         <select className="inp inp-select" value={item.tag} name="tag" onChange={(e) => setItem({ ...item, [e.target.name]: e.target.value })}>
                             <option value="User">User</option>
                             <option value="Requests">Requests</option>
                         </select>
                     </div>
-                    <div className='dv-mr'><label className='lb'>CATEGORY</label><input type="text" name="category" className='inp' value={item.category} onChange={(e) => setItem({ ...item, [e.target.name]: e.target.value })} /></div>
-                    <div className='dv-mr'><label className='lb'>DESCRIPTION</label><textarea type="text" name="description" className='inp txt-area' value={item.description} onChange={(e) => setItem({ ...item, [e.target.name]: e.target.value })} /></div>
+                    <div className='dv-mr'><label className='lb'>CATEGORY</label><input type="text" name="category" required className='inp' value={item.category} onChange={(e) => setItem({ ...item, [e.target.name]: e.target.value })} /></div>
+                    <div className='dv-mr'><label className='lb'>DESCRIPTION</label><textarea type="text" name="description" required className='inp txt-area' value={item.description} onChange={(e) => setItem({ ...item, [e.target.name]: e.target.value })} /></div>
                     <button className="lb btn" type="submit" value="CREATE" onClick={handleSubmit}>CREATE</button>
                 </div>
 
